09ae13af39489c5ca53645480269a62d8b08c8ed
Ruslan Andreev <rus.andreev@gmail.com>
2022-03-14 09:35:07 +0300

    Setup mocha tests with ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,19 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async () => {
+	// Get a list of all accounts
+	accounts = await web3.eth.getAccounts();
+
+	// Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+	it('deploys', () => {
+		console.log(accounts);
+	})
+})
